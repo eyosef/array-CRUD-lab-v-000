def76b168fa81c57a7b988fe712324418e658302
Eden Mesfun <emesfun@gmail.com>
2018-01-23 19:42:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   colors = ["blue", "yellow", "green", "black"]
-  colors << array
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
