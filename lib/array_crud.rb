0b2dd255741743eeb640eeba07adccc19878f3d9
Eden Mesfun <emesfun@gmail.com>
2018-01-23 19:39:04 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["blue", "yellow", "green", "black"]
+  colors = ["blue", "yellow", "green", "black"]
 end
 
 def add_element_to_end_of_array(array, element)
